e814df5332565587244ead3aecea6e4c264d0d3a
richardMeggyes <readdeo91@gmail.com>
2023-02-28 14:30:39 +0100

    Removed unused classes

--- src/main/java/hu/readdeo/money/management/svc/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package hu.readdeo.money.management.svc;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(collectionResourceRel = "test", path = "tests")
-public interface TestRepository extends PagingAndSortingRepository<Test, Long> {}
