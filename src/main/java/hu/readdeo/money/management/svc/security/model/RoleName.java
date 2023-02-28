1ca33adae312d137cbf38a5e8f76b1f0c029b249
readdeo91 <99890089+readdeo91@users.noreply.github.com>
2023-02-28 14:22:26 +0100

    Revert "[minor] Added the base of the project from Gogs."

--- src/main/java/hu/readdeo/money/management/svc/security/model/RoleName.java
+++
@@ -1,7 +0,0 @@
-package hu.readdeo.money.management.svc.security.model;
-
-public enum RoleName {
-  ROLE_USER,
-  ROLE_ADMIN,
-  ROLE_DEV
-}
--- src/main/java/hu/readdeo/money/management/svc/security/util/IAuthenticationFacade.java
+++
@@ -1,7 +0,0 @@
-package hu.readdeo.money.management.svc.security.util;
-
-import org.springframework.security.core.Authentication;
-
-public interface IAuthenticationFacade {
-  Authentication getAuthentication();
-}
